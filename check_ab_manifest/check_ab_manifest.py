ff415f68c9994fb5cd15c9173c78753925f92e6f
swejackies <398790400@qq.com>
2018-07-26 02:30:01 +0800

    add

---
+++ check_ab_manifest/check_ab_manifest.py
@@ -0,0 +1,27 @@
+#查找某个目录下的目标文件
+import os       #引入操作系统模块
+import sys      #用于标准输入输出
+import re
+
+#搜索Mainfest文件
+p1=re.compile("qq")
+def search_one_manifest_file(mainfest_file):
+    fileObj=open(mainfest_file)
+    for line in fileObj:
+        if p1.search(line):
+            print(mainfest_file)
+
+#搜索   
+def search_mainfest_file_in_working_dir():
+    for (root, dirs, files) in os.walk(os.getcwd()):
+        for filename in files:
+            filepath = os.path.join(root,filename)
+            #print(filepath)
+            if filepath.endswith(".manifest"):
+                search_one_manifest_file(filepath) 
+                 
+#Main              
+if __name__ == "__main__":
+    print('[Main][Begin]')
+    search_mainfest_file_in_working_dir()
+    print('[Main][End]')
